5e5ed6c4668895763a149c2373d9ae7fa97993fd
denis <deoniisii@gmail.com>
2018-12-02 23:28:22 +0200

    Crate MainActivity

---
+++ app/src/main/java/denisimus_it/net/timer/MainActivity.java
@@ -0,0 +1,13 @@
+package denisimus_it.net.timer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
