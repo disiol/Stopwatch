5010e7f8e7ea281e4daf051131edb1b13bdacdfb
denis <deoniisii@gmail.com>
2018-12-03 01:26:10 +0200

    make a show tamer

--- app/src/main/java/denisimus_it/net/timer/MainActivity.java
+++ app/src/main/java/denisimus_it/net/timer/MainActivity.java
@@ -1,13 +1,71 @@
 package denisimus_it.net.timer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView textViewShowTime;
+    private Button buttonStart, buttonStop, buttonReset;
+
+    private int seconds = 0;
+    private boolean isRunning = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textViewShowTime = findViewById(R.id.textViewShowTime);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonReset = findViewById(R.id.buttonReset);
+
+        buttonStart.setOnClickListener(this);
+        buttonStop.setOnClickListener(this);
+        buttonReset.setOnClickListener(this);
+
+        runTimer();
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.buttonStart:
+                isRunning = true;
+                break;
+            case R.id.buttonStop:
+                isRunning = false;
+                break;
+            case R.id.buttonReset:
+                isRunning = false;
+                seconds = 0;
+                break;
+        }
+    }
+
+    private void runTimer() {
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = seconds / 3600;
+                int minutes = (seconds % 3600) / 60;
+                int secs = seconds % 60;
+
+                String time = String.format(Locale.getDefault(), "%d:%02d:%02d", hours, minutes, secs);
+                textViewShowTime.setText(time);
+
+                if (isRunning) {
+                    seconds++;
+                }
+                handler.postDelayed(this, 1000);
+            }
+        });
     }
 }
